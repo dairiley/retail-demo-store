ff76d52cfdc4d902db4e6493afa1db33a1b798a2
Daisy Riley <rileydai@amazon.co.uk>
2023-07-07 15:23:02 +0100

    Create app.py to allow CDK deployment

---
+++ aws/cloudformation-templates/app.py
@@ -0,0 +1,9 @@
+from retail_demo_store import RetailDemoStoreStack
+from aws_cdk import App
+
+app = App()
+
+RetailDemoStoreStack(app, "retail-demo-store",
+                     description="RETAIL_DS: This deploys the Retail Demo Store reference architecture and workshop notebooks. (uksb-1t80l2nq1)")
+
+app.synth()
